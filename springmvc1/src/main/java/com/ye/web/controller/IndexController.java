0f7024b809284a1a23e90c08ada88b2530e7e7eb
yewei <yewei_258@163.com>
2020-02-01 20:47:04 +0800

    Signed-off-by: yewei <yewei_258@163.com>

---
+++ springmvc1/src/main/java/com/ye/web/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.ye.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    public String index(){
+        return "index";
+    }
+}
